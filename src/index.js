f29cc347db14b13ba791810bf563e118e644ab59
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2023-08-26 23:30:05 +0530

    refresh

--- src/index.js
+++ src/index.js
@@ -27,5 +27,6 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("connected to database"));
 
+
 // Start Server
 app.listen(port, () => console.log(`App listening on port ${port}!`));
