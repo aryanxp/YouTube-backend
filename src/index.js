61b2ae7158dccff11455c75f39fc2d3bd885c760
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2023-08-26 17:22:29 +0530

    added process env

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const app = require("./app.js");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+//DID NOT USE USERNAMER IN PROCESS.ENV BECAUSE IT WAS USERNAME OF COMPUTER
+const USERNAME = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL =
+  "mongodb+srv://" +
+  USERNAME +
+  ":" +
+  PASSWORD +
+  "@cluster0.vikdii3.mongodb.net/";
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on("error", (err) => console.log(err));
+db.on("error", (err) => console.log("Error connecting to database"));
 db.once("open", () => console.log("connected to database"));
 
 // Start Server
